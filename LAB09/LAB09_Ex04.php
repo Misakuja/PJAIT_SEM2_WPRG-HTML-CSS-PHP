be16a031b5b161185f0c6e4d63f3a396125d0a40
misakuja <s30284@pjwstk.edu.pl>
2024-05-12 12:05:28 +0200

    LAB09

--- LAB09/LAB09_Ex04.php
+++ LAB09/LAB09_Ex04.php
@@ -1,19 +1,19 @@
 <?php
-function reviewsReset() {
+function reviewsReset() : void{
     file_put_contents("LAB09_Ex04.txt", "");
 }
-function deleteReview() {
+function deleteReview() : void {
     $reviews = file("LAB09_Ex04.txt");
     unset($reviews[$_POST["index"]]);
 
     file_put_contents("LAB09_Ex04.txt", implode("", $reviews));
 }
-function addReview() {
+function addReview() : void {
     $reviewInput = $_POST["reviewInput"];
     $date = date("d-m-Y");
     $file = "LAB09_Ex04.txt";
 
-    file_put_contents($file, $date . "\n" . $reviewInput, FILE_APPEND);
+    file_put_contents($file, $date . " | " . $reviewInput . "\n", FILE_APPEND);
 }
 if(isset($_POST["delete"]) && isset($_POST["index"])) {
     deleteReview();
@@ -27,19 +27,20 @@ if(isset($_POST["reset"])) {
     reviewsReset();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Reviews Manager</title>
-    <link href="LAB09_Ex03.css" rel="stylesheet" type="text/css">
+    <link href="LAB09_Ex04.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div class="box">
+<div class="formResult">
+    <h1>Review Manager</h1>
+    <div class="form">
     <form method='post' action="">
         <fieldset>
-            <legend>Managing Reviews</legend>
+            <legend>Add Review</legend>
             <div class=formWrap>
                 <label for='reviewInput'></label>
                 <input type='text' id='reviewInput' name='reviewInput' placeholder="Write your review here." required>
@@ -48,13 +49,19 @@ if(isset($_POST["reset"])) {
             </div>
         </fieldset>
     </form>
+    </div>
     <div class="reviews">
         <h1>Reviews:</h1>
         <?php
             if(file_exists("LAB09_Ex04.txt")) {
                 $reviewsArray = file("LAB09_Ex04.txt");
                 foreach($reviewsArray as $index => $review) {
-                    echo "<div>$review<form action='' method='post'><input type='hidden' name='index' value='$index'><input type='submit' name='delete' value='Delete'></form></div><br>";
+                    echo "<div class='border'>$review
+                    <form action='' method='post'>
+                    <input type='hidden' name='index' value='$index'>
+                    <input type='submit' name='delete' value='Delete'>
+                    </form>
+                    </div><br>";
                 }
             }
         ?>
